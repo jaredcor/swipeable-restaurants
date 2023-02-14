0666ea3434c8d48eed4bc052193adeccd40fe737
jaredcor <jaredbcortez@outlook.com>
2023-02-14 14:00:43 -0600

    Initial Commit

---
+++ swipeable-restaurants/swipeable_restaurantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swipeable_restaurantsApp.swift
+//  swipeable-restaurants
+//
+//  Created by Jared Cortez on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct swipeable_restaurantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
